3f4876397141e6047db03be3f675f89f79f1b155
jeong min <jjgmt0917@gmail.com>
2023-02-07 22:51:17 +0900

    add req,res

--- src/server.js
+++ src/server.js
@@ -3,7 +3,13 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {return res.end()};
+const handleLogin = (req, res) => {return res.end("Login here.")};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
 const handleListening = () => console.log('Server listening on port http://localhost:4000 🚀');
 
-app.listen(PORT, handleListening)
+app.listen(PORT, handleListening);
 
