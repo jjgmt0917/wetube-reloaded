e5b410156494ba92215f80bf89982eecd702a93a
jeong min <jjgmt0917@gmail.com>
2023-03-07 23:54:07 +0900

    add morgan and README

--- src/server.js
+++ src/server.js
@@ -1,15 +1,24 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
+
 const app = express();
+const logger = morgan("dev");
 
-const handleHome = (req, res) => {return res.end()};
-const handleLogin = (req, res) => {return res.end("Login here.")};
+const home = (req, res ) => {
+    console.log("I will respond.");
+    return res.send("hello");
+};
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+const login = (req, res) => {
+    return res.send("login");
+};
 
-const handleListening = () => console.log('Server listening on port http://localhost:4000 🚀');
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
+
+const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}🚀`);
 
 app.listen(PORT, handleListening);
-
