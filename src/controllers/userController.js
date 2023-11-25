e885d7c09f20f702ace3353700fb068904f694b3
jeong min <jjgmt0917@gmail.com>
2023-11-25 19:44:00 +0900

    update route

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,6 @@
 export const join = (req, res) => res.send("Join");
+export const see = (req, res) => res.send("My Profile");
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
+export const login = (req, res) => res.send("Log In");
+export const logout = (req, res) => res.send("Log Out");
