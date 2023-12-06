bcbb415f20a2a06ccf55b619294f9b36ac5a146e
jeong min <jjgmt0917@gmail.com>
2023-12-07 04:35:26 +0900

    "post" and "get"

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,26 +1,57 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,
+    },
+    {
+        title: "Second Video",
+        rating: 1,
+        comments: 45,
+        createdAt: "10 minutes ago",
+        views: 156,
+        id: 2,
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 1,
+        id: 3,
+    },
+    {
+        title: "Fourth Video",
+        rating: 1,
+        comments: 45,
+        createdAt: "10 minutes ago",
+        views: 156,
+        id: 4,
+    },
+];
+
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: "First Video",
-            rating: 5,
-            comments: 2,
-            createdAt: "2 minutes ago",
-            views: 59,
-            id: 1,
-        },
-        {
-            title: "Second Video",
-            rating: 1,
-            comments: 45,
-            createdAt: "10 minutes ago",
-            views: 156,
-            id: 8,
-        }
-    ]
     return res.render("home", { pageTitle: "Home", videos })
 };
-export const see = (req, res) => res.render("watch", { pageTitle: "Video"});
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit"});
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    return res.render("edit", { pageTitle: `Editing : ${video.title}`, video });
+};  //form 화면을 보여줌
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+};  //변경사항을 저장
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
