36a727e9c07ebb4be07fae5cb9bc19d9e8a32643
jeongmin <jjgmt0917@gmail.com>
2024-05-28 16:50:20 +0900

    update 'video edit'

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,4 @@
+import { format } from "morgan";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
@@ -5,37 +6,34 @@ export const home = async (req, res) => {
     return res.render("home", { pageTitle : "Home", videos });
         
 };//최신버전
-/* export const home = (req, res) => {
-    console.log("Start");
-    Video.find().
-    then(function (videos){
-    console.log("Finished", videos);
-    return res.render("home", { pageTitle: "Home", videos });
-    })
-    .catch(function (err) {
-    console.log("errors", err);
-    });
-    console.log("I finish first");
-    }; //callback함수 에러 해결. mongoose업데이트 문제 */
-/* export const home = (req, res) => {
-    console.log("Start");
-    Video.find({}, (error, videos) => {
-        return res.render("home", { pageTitle: "Home", videos });
-    });
-}; // 콜백방식 */
+
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    console.log(video);
+    if( !video ){
+        return res.render("404", { pageTitle: "Video not found."});
+    };
     return res.render("watch", { pageTitle: video.title, video });
 };
-export const getEdit = (req, res) => {
+export const getEdit = async(req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing` });
+    const video = await Video.findById(id);
+    if( !video ){
+        return res.render("404", { pageTitle: "Video not found."});
+    };
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video });
 };  //form 화면을 보여줌
-export const postEdit = (req, res) => {
+export const postEdit = async (req, res) => {
     const { id } = req.params;
-    const { title } = req.body;
+    const { title, description, hashtags} = req.body;
+    const video = await Video.exists({ _id: id });
+    if( !video ){
+        return res.render("404", { pageTitle: "Video not found."});
+    };
+    await Video.findByIdAndUpdate(id, {
+        title,description,
+        hashtags: Video.formatHashtags(hashtags),
+    });
     return res.redirect(`/videos/${id}`);
 };  //변경사항을 저장
 
@@ -49,7 +47,7 @@ export const postUpload = async(req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map((word) => `#${word}`),
+            hashtags: Video.formatHashtags(hashtags),
         });
         return res.redirect("/");
     } catch(error){
